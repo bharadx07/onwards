0e6958a76166cdc9f26910877dc663a4a4123099
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2022-03-10 21:39:05 -0800

    feat: proto server and client handle files

--- prototype/index.js
+++ prototype/index.js
@@ -25,33 +25,11 @@ app.get("/", (req, res) => {
 });
 
 app.post("/upload-file", async (req, res) => {
-  try {
-    if (!req.files) {
-      res.send({
-        status: false,
-        message: "No file uploaded",
-      });
-    } else {
-      //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-      let avatar = req.files.avatar;
+  console.log(req.files);
 
-      //Use the mv() method to place the file in upload directory (i.e. "uploads")
-      avatar.mv("./uploads/" + avatar.name);
+  const pic = req.files["files[]"];
 
-      //send response
-      res.send({
-        status: true,
-        message: "File is uploaded",
-        data: {
-          name: avatar.name,
-          mimetype: avatar.mimetype,
-          size: avatar.size,
-        },
-      });
-    }
-  } catch (err) {
-    res.status(500).send(err);
-  }
+  pic.mv("./uploads/" + pic.name);
 });
 
 app.listen(8080, () => {
